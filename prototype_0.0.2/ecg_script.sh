838c4092598a4e3b5c71d562d56a7bc56b926f1b
com-pass-ion <nicojosefhechler@gmail.com>
2026-05-19 22:25:08 +0200

    Update ecg_script.sh
    - clean up
    - parsing: sed -> pure bash

--- prototype_0.0.2/ecg_script.sh
+++ prototype_0.0.2/ecg_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[[ "$1" == "-d" ]] && DEBUG_FLAG=1 || DEBUG_FLAG=0
+
 POLAR_MAC="24:AC:AC:14:D4:93"
 PMD_CONTROL_UUID="fb005c81-02e7-f387-1cad-8acd2d8df0c8"
 PMD_DATA_UUID="fb005c82-02e7-f387-1cad-8acd2d8df0c8"
@@ -37,29 +39,41 @@ cleanup() {
 to_coproc() {
     local cmd="$1"
     local delay="$2"
-    # echo "$cmd" >& "${BLUETOOTHCTL[1]}";
+    if (( DEBUG_FLAG ));then
+	echo "[DEBUG]: $cmd" >&2 "${BLUETOOTHCTL[1]}";
+    fi
     sleep "$delay";
 }
 
+REGEX_FULL_LINE="([a-f0-9]{2} ){15}[a-f0-9]{2}"
+REGEX_LAST_LINE="([a-f0-9]{2} ){3}[a-f0-9]{2}"
 
 ############################################################
 #  MAIN
 ############################################################
-echo "❤️ Initializing Polar H10 RAW ECG Stream via Bash coproc..."
-echo "Press Ctrl+C at any time to exit safely."
+if (( DEBUG_FLAG ));then
+    echo "[--- DEBUG MODE ---]"
+    echo "❤️ Initializing Polar H10 RAW ECG Stream via Bash coproc..."
+    echo "Press Ctrl+C at any time to exit safely."
+fi
 
 coproc BLUETOOTHCTL { bluetoothctl; }
+sleep 1
 
 trap "cleanup 2" INT TERM
 
-to_coproc "connect $POLAR_MAC" 10
+to_coproc "connect $POLAR_MAC" 3
 
 for cmd in "${start_stream[@]}"; do
-    to_coproc "$cmd" 3
+    to_coproc "$cmd" 0.5
 done
 
-echo "📡 Streaming active. Listening for incoming data..."
+if (( DEBUG_FLAG ));then
+    echo "📡 Streaming active. Listening for incoming data..."
+fi
 
 while read -r line <& "${BLUETOOTHCTL[0]}"; do
-    echo "$line" | sed -E -e '/^ *$/d' -e '/Value:/d' -e 's/  +([^ ]*)$//';
+    if [[ "$line" =~ $REGEX_FULL_LINE || "$line" =~ $REGEX_LAST_LINE ]]; then
+	echo "${BASH_REMATCH[0]}"
+    fi
 done
